c1c0877d825514841f8ee01c7f355e4fb7b9c660
Augusto Mello <augustomello@MacBook-Air-de-Augusto.local>
2026-05-31 12:33:54 -0300

    fix(configs): Added correct method to obtain the SECRET_KEY from the .env

--- configs/settings.py
+++ configs/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
 allowed_hosts = os.environ.get("ALLOWED_HOSTS").split(",")
 
-ALLOWED_HOSTS = [host.strip for host in allowed_hosts if host.strip()]
+ALLOWED_HOSTS = [host.strip() for host in allowed_hosts if host.strip()]
 
 # Application definition
 
