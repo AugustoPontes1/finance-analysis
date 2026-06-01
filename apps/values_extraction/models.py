7ca735649acd3e416e8a47e1883d30e532e2166e
Augusto Mello <augustomello@MacBook-Air-de-Augusto.local>
2026-06-01 08:39:57 -0300

    feat(values_extraction): Added currency_princ and currency_type for the DocumentModel

--- apps/values_extraction/models.py
+++ apps/values_extraction/models.py
@@ -34,6 +34,9 @@ class DocumentModel(models.Model):
         verbose_name="file_content", 
         blank=False, null=False
     )
+    currency_price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency_type = models.CharField(max_length=3, default='BRL')
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
