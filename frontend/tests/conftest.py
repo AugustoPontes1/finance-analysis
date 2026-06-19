a81e49993aa654eee790ff5f26beedcbd95f616d
Augusto Pontes <augustopontes010@gmail.com>
2026-06-19 14:37:17 -0300

    test(frontend): Added conftest parameters

--- frontend/tests/conftest.py
+++ frontend/tests/conftest.py
@@ -1,2 +1,24 @@
 import pytest
-from playwright.sync_api
+from playwright.sync_api import sync_playwright
+
+import os
+
+BASE_URL_FRONT = os.getenv("BASE_URL_FRONT")
+
+
+@pytest.fixture(scope="session")
+def browser():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        yield browser
+        browser.close()
+
+@pytest.fixture
+def page(browser):
+    page = browser.new_page()
+    page.goto(BASE_URL_FRONT)
+    page.wait_for_load_state("networkidle")
+    yield page
+    page.close()
+
+
